e1de3bcc5eb625b9f22d828689fe2717b75d7851
Vitor Ramos <vitor_rolim11@hotmail.com.br>
2024-09-25 19:00:09 -0300

    Criando a classe para o catálogo de jogos

---
+++ CatalogoJogos.cs
@@ -0,0 +1,30 @@
+﻿class CatalogoJogos
+{
+    public CatalogoJogos(string nome, string genero, int anoLanc, List<string> jogos)
+    {
+        Nome = nome;
+        Genero = genero;
+        AnoLanc = anoLanc;
+        Jogos = jogos;
+    }
+    public string Nome { get; set; }
+    public string Genero { get; set; }
+    public int AnoLanc { get; set; }
+
+    public List<string> Jogos { get; set; } = new List<string>();
+
+    public void AdicionarJogos(string nome)
+    {
+        Jogos.Add(nome);
+        Console.WriteLine($"O jogo {nome} foi adicionado com sucesso!");
+    }
+
+    public void ExibirJogos()
+    {
+        Console.WriteLine("Catálogo de jogos: ");
+        foreach (string jog in Jogos)
+        {
+            Console.WriteLine($"Jogo: {jog}");
+        }
+    }
+}
