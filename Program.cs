725ac2ff1a3b3adaf5736d4846d283ecbd6ad5c8
Vitor Ramos <vitor_rolim11@hotmail.com.br>
2024-09-25 19:01:02 -0300

    Instanciando a classe e adicionando jogos para a lista

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿List<string> ctlgJogos = new List<string>();
+
+Console.Write("Digite o nome do jogo: ");
+string nome = Console.ReadLine();
+Console.Write("Digite o genero do jogo: ");
+string genero = Console.ReadLine();
+Console.Write("Digite o ano do lançamento: ");
+int anoLanc = int.Parse(Console.ReadLine());
+CatalogoJogos jogo1 = new CatalogoJogos(nome, genero, anoLanc, ctlgJogos);
+
+Console.Write("Digite o nome do jogo: ");
+string nome2 = Console.ReadLine();
+Console.Write("Digite o genero do jogo: ");
+string genero2 = Console.ReadLine();
+Console.Write("Digite o ano do lançamento: ");
+int anoLanc2 = int.Parse(Console.ReadLine());
+
+CatalogoJogos jogo2 = new CatalogoJogos(nome2, genero2, anoLanc2, ctlgJogos);
+ 
+ 
+
+
+jogo1.AdicionarJogos(nome);
+jogo2.AdicionarJogos(nome2);
+jogo2.ExibirJogos();
